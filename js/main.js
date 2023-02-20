e610c53a210ea122445f78040c76d7baf3cba3f2
julucca <lucca.editora@gmail.com>
2023-02-20 09:39:24 -0300

    feat: create part button logic

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const controles = document.querySelectorAll('.controle-ajuste');
+
+controles.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
+    })
+})
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('.controle-contador');
+
+    if(operacao === '-') {
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
